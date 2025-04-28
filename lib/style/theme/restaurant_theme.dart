4b643276a573e360361fa51d05264a5f4b67f94d
rizki <mohamadrizki8@gmail.com>
2025-04-28 11:44:53 +0700

    feat: Implement Restaurant Theme and Typography
    This commit introduces a new theming system for the application, including color and text styles.
    
    -   Added `RestaurantTheme` to define light and dark themes.
    -   Implemented `RestaurantTextStyles` to manage text styles, including display, headline, title, body, and label variants.
    -   Created `RestaurantColors` to manage app color.
    - Updated `main.dart` to use `RestaurantTheme`.
    - Updated dependencies in `pubspec.yaml` and `pubspec.lock`.
    - Reorganize `restaurant_colors.dart` under colors folder.

---
+++ lib/style/theme/restaurant_theme.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant/style/colors/restaurant_colors.dart';
+
+import '../typograhy/restaurant_text_styles.dart';
+
+class RestaurantTheme {
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      colorSchemeSeed: RestaurantColors.red.color,
+      brightness: Brightness.light,
+      textTheme: _textTheme,
+      useMaterial3: true,
+      appBarTheme: _appBarTheme,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      colorSchemeSeed: RestaurantColors.red.color,
+      brightness: Brightness.dark,
+      textTheme: _textTheme,
+      useMaterial3: true,
+      appBarTheme: _appBarTheme,
+    );
+  }
+
+  static TextTheme get _textTheme {
+    return TextTheme(
+      displayLarge: RestaurantTextStyles.displayLarge,
+      displayMedium: RestaurantTextStyles.displayMedium,
+      displaySmall: RestaurantTextStyles.displaySmall,
+      headlineLarge: RestaurantTextStyles.headlineLarge,
+      headlineMedium: RestaurantTextStyles.headlineMedium,
+      headlineSmall: RestaurantTextStyles.headlineSmall,
+      titleLarge: RestaurantTextStyles.titleLarge,
+      titleMedium: RestaurantTextStyles.titleMedium,
+      titleSmall: RestaurantTextStyles.titleSmall,
+      bodyLarge: RestaurantTextStyles.bodyLargeBold,
+      bodyMedium: RestaurantTextStyles.bodyLargeMedium,
+      bodySmall: RestaurantTextStyles.bodyLargeRegular,
+      labelLarge: RestaurantTextStyles.labelLarge,
+      labelMedium: RestaurantTextStyles.labelMedium,
+      labelSmall: RestaurantTextStyles.labelSmall,
+    );
+  }
+
+  static AppBarTheme get _appBarTheme {
+    return AppBarTheme(
+        toolbarTextStyle: _textTheme.titleLarge,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
+        )
+    );
+  }
+}
